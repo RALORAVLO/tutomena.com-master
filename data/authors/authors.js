d9ba002f10eb6246aee81db8cb8f147262a64064
RALO <62395148+RALORAVLO@users.noreply.github.com>
2020-12-09 14:56:28 -0600

    Initial commit

---
+++ data/authors/authors.js
@@ -0,0 +1,31 @@
+import photo_aissa from './avatars/aissa.png';
+import photo_djafer from './avatars/djafer.jpg';
+
+const aissa = {
+  name: 'عيسى محمد علي',
+  photo: photo_aissa,
+  bio:
+    'مغربي من أصول أمازيغية، مطور ويب مستقل متخصص في الواجهات الأمامية، أحب التدوين وإغناء المحتوى التقني للغة الضاد وهذا كان السبب الرئيسي في إنشائي لمدونة توتومينا.',
+  website: '',
+  social: {
+    facebook: '',
+    twitter: '',
+    github: 'https://github.com/aissa-bouguern',
+    linkedin: 'https://linkedin.com/in/bouguern'
+  }
+};
+
+const djafer = {
+  name: 'عبدالهادي جعفر',
+  photo: photo_djafer,
+  bio:
+    'مصمم ومطور مشاريع رقمية (Digital Product Designer) ومهتم بتجربة المستخدم UX/UI',
+  website: '',
+  social: {
+    facebook: '',
+    twitter: 'https://twitter.com/elh4di',
+    github: 'https://github.com/asotjrs',
+    linkedin: 'https://linkedin.com/in/djafer'
+  }
+};
+export default { aissa, djafer };
